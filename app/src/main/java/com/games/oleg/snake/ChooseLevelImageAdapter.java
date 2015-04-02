42a6dbce33ff70a23c5509edfcf8ac15fe4bdf0e
Hjelm <onshlem@gmail.com>
2015-04-02 17:52:56 +0300

    Drawing level number on level icon

--- app/src/main/java/com/games/oleg/snake/ChooseLevelImageAdapter.java
+++ app/src/main/java/com/games/oleg/snake/ChooseLevelImageAdapter.java
@@ -1,11 +1,16 @@
 package com.games.oleg.snake;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by oleg.shlemin on 24.03.2015.
@@ -31,6 +36,7 @@ public class ChooseLevelImageAdapter extends BaseAdapter{
         return 0;
     }
 
+    /*
     // create a new ImageView for each item referenced by the Adapter
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imageView;
@@ -47,5 +53,34 @@ public class ChooseLevelImageAdapter extends BaseAdapter{
         imageView.setImageResource(R.drawable.blue_square);
         return imageView;
     }
+*/
 
+    // create a new ImageView for each item referenced by the Adapter
+    @TargetApi(16)
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView textView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            textView = new TextView(mContext);
+            textView.setLayoutParams(new GridView.LayoutParams(200, 200));
+            int sdk = android.os.Build.VERSION.SDK_INT;
+            Drawable levelIcon = parent.getResources().getDrawable(R.drawable.blue_square);
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                textView.setBackgroundDrawable(levelIcon);
+            } else {
+                textView.setBackground(levelIcon);
+            }
+            textView.setText(Integer.toString(position+1));
+            textView.setTextColor(Color.RED);
+            textView.setGravity(Gravity.CENTER);
+            textView.setTextSize(20);
+            //textView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            textView.setPadding(8, 8, 8, 8);
+        } else {
+            textView = (TextView) convertView;
+        }
+
+        //imageView.setImageResource(R.drawable.blue_square);
+        return textView;
+    }
 }
